5b641760d7ee29167bc85af5654875fce24c2589
PabloHBMiranda <pablo.hbmiranda@gmail.com>
2024-04-07 21:24:17 -0300

    Configuração das bullets

--- player/player.gd
+++ player/player.gd
@@ -2,9 +2,14 @@ extends AnimatedSprite2D
 
 # Declara uma variável de velocidade inicialmente parada
 var velocity = Vector2(0, 0)
+var can_shoot = true
 
 # Define uma constante de velocidade máxima
-const SPEED = Vector2(100, 90)
+const SPEED = Vector2(125, 90)
+const BULLET_OFFSET = 7
+const Bullet = preload("res://player/player_bullet/player_bullet.tscn")
+
+@onready var reaload_timer = $ReloadTimer
 
 func _process(delta):
 	# Define a velocidade horizontal baseada na entrada do jogador
@@ -21,7 +26,24 @@ func _process(delta):
 		flip_h = false  # Se sim, o sprite se mantém "olhando" para a direita
 	elif velocity.x < 0:  # Verifica se o movimento é para a esquerda
 		flip_h = true  # Se sim, o sprite "olha" para a esquerda
+	
+	if Input.is_action_pressed("shoot") and can_shoot == true:
+		var bullet_instance = Bullet.instantiate()
+		bullet_instance.global_position = global_position
+		get_tree().current_scene.add_child(bullet_instance)
+		
+		if flip_h == true:
+			bullet_instance.flip_direction()
+			bullet_instance.global_position = global_position - Vector2(BULLET_OFFSET, 0)
+		else:
+			bullet_instance.global_position = global_position + Vector2(BULLET_OFFSET, 0)
+		
+		reaload_timer.start()
+		can_shoot = false
 
 func _physics_process(delta):
 	# Move o objeto no jogo de acordo com sua velocidade e tempo passado
 	global_position += velocity * SPEED  * delta  
+
+func _on_reload_timer_timeout():
+	can_shoot = true	
--- player/player_bullet/player_bullet.gd
+++ player/player_bullet/player_bullet.gd
@@ -4,6 +4,16 @@ var velocity = Vector2(1, 0)
 
 var SPEED = 300
 
+func _ready():
+	rotation_degrees = randf_range(-7, 7)
+	velocity = velocity.rotated(rotation)
+
 func _process(delta):
 	global_position += velocity * SPEED * delta
+
+func flip_direction():
+	velocity = -velocity
+	flip_h = !flip_h
 	
+func _screen_exited():
+	queue_free()
