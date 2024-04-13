8d865ffc46e070413734b002a195f85def5e1c19
PabloHBMiranda <pablo.hbmiranda@gmail.com>
2024-04-13 12:29:40 -0300

    add: spawn enemys

--- enemies/enemy_spawner/enemy_spawner.gd
+++ enemies/enemy_spawner/enemy_spawner.gd
@@ -1,9 +1,25 @@
-extends Marker2D
+extends Node2D
 
 const Shark = preload("res://enemies/shark/shark.tscn")
 
-func _on_timer_timeout():
-	var shark_instance = Shark.instantiate()
-	get_tree().current_scene.add_child(shark_instance)
-	shark_instance.global_position = global_position
+@onready var left = $Left
+@onready var right = $Right	
+ 
+func _on_spawn_enemy_timer_timeout():
+	var rand_spawn_point_number = randi_range(1, 4)
 	
+	var selected_side_node = left
+	var spawn_right = bool(randi_range(0, 1))
+	
+	if spawn_right == true:
+		selected_side_node = right
+	
+	var selected_spawn_point = selected_side_node.get_node(str(rand_spawn_point_number))
+	var spaw_position = selected_spawn_point.global_position
+	
+	var shark_instance = Shark.instantiate()
+	shark_instance.global_position = spaw_position
+	get_tree().current_scene.add_child(shark_instance)
+	
+	if spawn_right == true:
+		shark_instance.flip_direction()
--- enemies/shark/shark.gd
+++ enemies/shark/shark.gd
@@ -2,23 +2,24 @@ extends Area2D
 
 const SPEED = 50
 const MOVEMENT_FREQUENCY = 0.15
-const MOVEMENTE_AMPLITUDE = 0.5
+const MOVEMENTE_AMPLITUDE = 0.5  
 
 var velocity = Vector2(1, 0)
+
+@onready var sprite = $AnimatedSprite2D
 
 func _physics_process(delta):
 	velocity.y = sin(global_position.x * MOVEMENT_FREQUENCY) * MOVEMENTE_AMPLITUDE
 	global_position += velocity * SPEED * delta
 
+func flip_direction():
+	velocity = -velocity
+	sprite.flip_h = !sprite.flip_h
+
 func _on_area_entered(area):
 	if area.is_in_group("PlayerBullet"):
 		area.queue_free()
 		queue_free()
-
-
-func _on_visible_on_screen_notifier_2d_screen_exited():
-	pass # Replace with function body.
-
 
 func _on_screen_exited():
 	queue_free()
