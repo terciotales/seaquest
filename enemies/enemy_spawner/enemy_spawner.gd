73a6e5c77f6c6e98c81648f49f7bdf660a37d4cf
PabloHBMiranda <pablo.hbmiranda@gmail.com>
2024-04-13 18:28:30 -0300

    finaliza spawn de tubarões

--- enemies/enemy_spawner/enemy_spawner.gd
+++ enemies/enemy_spawner/enemy_spawner.gd
@@ -2,11 +2,29 @@ extends Node2D
 
 const Shark = preload("res://enemies/shark/shark.tscn")
 
+var used_spawn_points = []
+
 @onready var left = $Left
 @onready var right = $Right	
- 
+
+# nova onda spawnada
 func _on_spawn_enemy_timer_timeout():
-	var rand_spawn_point_number = randi_range(1, 4)
+	for i in range(4):
+		spawn_enemy()
+	
+	used_spawn_points.clear()
+
+# apenas um inimigo spawnado
+func spawn_enemy():
+	var available_spawn_points = []
+	
+	for i in range(1, 5):
+		if !used_spawn_points.has(i):
+			available_spawn_points.append(i)
+	
+	var rand_spawn_point_index = randi_range(0, available_spawn_points.size() - 1)
+	var selected_spawn_point_number = available_spawn_points[rand_spawn_point_index]
+	used_spawn_points.append(selected_spawn_point_number)
 	
 	var selected_side_node = left
 	var spawn_right = bool(randi_range(0, 1))
@@ -14,8 +32,8 @@ func _on_spawn_enemy_timer_timeout():
 	if spawn_right == true:
 		selected_side_node = right
 	
-	var selected_spawn_point = selected_side_node.get_node(str(rand_spawn_point_number))
-	var spaw_position = selected_spawn_point.global_position
+	var selected_spawn_point = selected_side_node.get_node(str(selected_spawn_point_number))
+	var spaw_position = selected_spawn_point.global_position 
 	
 	var shark_instance = Shark.instantiate()
 	shark_instance.global_position = spaw_position
--- enemies/shark/shark.gd
+++ enemies/shark/shark.gd
@@ -5,11 +5,12 @@ const MOVEMENT_FREQUENCY = 0.15
 const MOVEMENTE_AMPLITUDE = 0.5  
 
 var velocity = Vector2(1, 0)
+var random_offset = randf_range(0, 10)
 
 @onready var sprite = $AnimatedSprite2D
 
 func _physics_process(delta):
-	velocity.y = sin(global_position.x * MOVEMENT_FREQUENCY) * MOVEMENTE_AMPLITUDE
+	velocity.y = sin(global_position.x * MOVEMENT_FREQUENCY + random_offset) * MOVEMENTE_AMPLITUDE
 	global_position += velocity * SPEED * delta
 
 func flip_direction():
